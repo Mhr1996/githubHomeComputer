1269dd22e6accade797d9cbe8085f9f1f99e1c84
MaHongRui <838684454@qq.com>
2018-09-14 00:39:32 +0800

    2018-09-14 差17分钟

---
+++ reactworld/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+/*import App from './App';*/
+import Welcome from './welcome';
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
---
+++ reactworld/src/welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+	render(){
+		var list=[1,2,3,4];
+		var default_=true;
+		return (
+			<div>
+				<h1>Heloise</h1>
+			</div>
+		)
+	}
+}
+
+export default Welcome
