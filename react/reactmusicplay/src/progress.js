a95a14def0dc8b55a4968b9fb4337ef2e0e251ec
MaHongRui <838684454@qq.com>
2018-10-17 21:37:27 +0800

    test

--- react/reactmusicplay/src/progress.js
+++ react/reactmusicplay/src/progress.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Progress extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.changeprogress=this.changeprogress.bind(this);
+	}
+	changeprogress(e){
+		let prB=this.refs.prB; //原生的dom节点 通过设置div的ref 
+		let progress=(e.clientX - prB.getBoundingClientRect().left) / prB.clientWidth;
+		this.props.childrenProgress(progress)
+	}
 	render(){
 		return(
-			<div>
-				{this.props.progress}S
+			<div className="progress" onClick={this.changeprogress} ref="prB">
+				<div className="progressBar" style={{width:`${this.props.progress}%`}}></div>
 			</div>
 		)
 	}
--- react/reactmusicplay/src/root.js
+++ react/reactmusicplay/src/root.js
@@ -3,6 +3,7 @@ import Header from './header';
 import Progress from './progress';
 import  jPlayer  from 'jplayer';
 
+let musicTime=null;
 class Root extends React.Component{
 	constructor(props){
 		super(props);
@@ -10,12 +11,14 @@ class Root extends React.Component{
 		this.state={
 			progress:'-'
 		}
+
+		this.modifyProgress=this.modifyProgress.bind(this);
 	}
 	componentDidMount(){
 		$("#player").jPlayer({
 			ready:function(){
 				$(this).jPlayer('setMedia',{
-					mp3: 'https://m10.music.126.net/20181016213546/b6227f5a30c08879c30f4ee81d2d288e/ymusic/f7d9/3a9e/92a2/0515ecaeb8adcecb2e98634c7e543cf2.mp3'
+					mp3: 'https://m10.music.126.net/20181017205644/278ca56e4938bae44fca54390c801ffb/ymusic/5d63/5150/0851/5f226aac018cafc2cb248f7d28fbd5b4.mp3'
 				}).jPlayer('play');
 			},
 			supplied:'mp3',
@@ -23,15 +26,22 @@ class Root extends React.Component{
 		});
 
 		$("#player").bind($.jPlayer.event.timeupdate, (e) => {
+			musicTime=e.jPlayer.status.duration;
 			this.setState({
-				progress:Math.round(e.jPlayer.status.currentTime)
+				progress:e.jPlayer.status.currentPercentAbsolute
 			});
 		})
+	}
+	componentWillUnMount(){
+		$("#player").unbind($.jPlayer.event.timeupdate);
+	}
+	modifyProgress(second){
+		$("#player").jPlayer('play', musicTime * second);
 	}
 	render(){
 		return(
 			<div>
-				<Header /><Progress progress={this.state.progress}/>
+				<Header /><Progress progress={this.state.progress} childrenProgress={this.modifyProgress}/>
 				<div id="player">33</div>
 			</div>
 		)
